2ea2de74dd1ac5f1aafc945499c3d0126ee6f73e
kuma <kumamonmaster@gmail.com>
2017-01-29 01:12:52 +0900

    complete the lesson14 of part2

--- data.php
+++ data.php
@@ -6,4 +6,6 @@ $coffee = new Menu('COFFEE', 500, "https://s3-ap-northeast-1.amazonaws.com/proga
 $curry = new Menu('CURRY', 900, "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png");
 $pasta = new Menu('PASTA', 1200, "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png");
 
+$juice->setOrderCount(2);
+
 $menus = array($juice, $coffee, $curry, $pasta);
--- index.php
+++ index.php
@@ -17,6 +17,7 @@
           <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
           <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
           <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?>（税込）</p>
+          <p>注文数: <?php echo $menu->getOrderCount() ?></p>
         </div>
       <?php endforeach ?>
     </div>
