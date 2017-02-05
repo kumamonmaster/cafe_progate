ef594a4da5d43775b542a071cd4725933538fc4e
kuma <kumamonmaster@gmail.com>
2017-02-05 18:46:22 +0900

    Complete the lesson16 of part2

--- confirm.php
+++ confirm.php
@@ -1,10 +1,28 @@
+<!-- ここでdata.phpを読み込んでください  -->
+<?php
+require_once('data.php');
+?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="order-wrapper">
+    <h2>注文内容確認</h2>
+    <?php foreach ($menus as $menu): ?>
+      <?php $orderCount = $_POST[$menu->getName()] ?>
+      <p class="order-amount">
+        <?php echo $menu->getName() ?>
+        x
+        <?php echo $orderCount ?>
+        個
+      </p>
+    <?php endforeach ?>
+  </div>
+</body>
 </html>
